855b0ae21e2a217beb1bf10f92e631ff742a3d47
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-11 16:41:27 +0100

    Korisnik_Suplement Endpoint Controlleri

---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Suplement/GetAll/Korisnik_SuplementGetAllEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Entities.Endpoint.KorisnikSuplement.GetAll
+{
+    [Route("Korisnik_Suplement-GetAll")]
+
+    public class Korisnik_SuplementGetAllEndpoint : MyBaseEndpoint<Korisnik_SuplementGetAllRequest, Korisnik_SuplementGetAllResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public Korisnik_SuplementGetAllEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public override async Task<Korisnik_SuplementGetAllResponse> Handle([FromQuery]Korisnik_SuplementGetAllRequest request)
+        {
+            var korisniksuplement = await db.Korisnik_Suplement
+                .Select(x => new KorisnikSuplementGetAllResponseRow
+                {
+                    SuplementiID=x.SuplementID,
+                    KorisnikID=x.KorisnikID,
+                    DatumVrijemeNarudzbe=x.DatumVrijemeNarudzbe,
+                    Kolicina=x.Kolicina,
+                  
+                }).ToListAsync();
+
+            return new Korisnik_SuplementGetAllResponse
+            {
+                KorisnikSuplement = korisniksuplement
+            };
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Suplement/GetAll/Korisnik_SuplementGetAllResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace GMS.Entities.Endpoint.KorisnikSuplement.GetAll
+{
+    public class Korisnik_SuplementGetAllResponse
+    {
+        public List<KorisnikSuplementGetAllResponseRow> KorisnikSuplement { get; set; }
+    }
+
+    public class KorisnikSuplementGetAllResponseRow
+    {
+        public int KorisnikID { get; set; }
+        public int SuplementiID { get; set; }
+        public DateTime DatumVrijemeNarudzbe { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
