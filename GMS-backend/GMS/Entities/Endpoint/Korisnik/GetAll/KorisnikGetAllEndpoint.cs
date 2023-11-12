e8cc4924bdc80adb6590184b4bb5b452a3ebb82f
Vedad <vedad.ribery@gmail.com>
2023-11-12 01:31:43 +0100

    Korisnik endpoint

---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik/GetAll/KorisnikGetAllEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Entities.Endpoint.Korisnik.GetAll
+{
+    [Route("Korisnik-GetAll")]
+
+    public class KorisnikGetAllEndpoint : MyBaseEndpoint<KorisnikGetAllRequest, KorisnikGetAllResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public KorisnikGetAllEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public override async Task<KorisnikGetAllResponse> Handle([FromQuery] KorisnikGetAllRequest request)
+        {
+            var korisnici = await db.Korisnik
+                .Select(x => new KorisnikGetAllResponseRow
+                {
+                   ID = x.ID,
+                   Ime = x.Ime,
+                   Prezime = x.Prezime,
+                   Username = x.Username,
+                   Password = x.Password,
+                   Slika = x.Slika,
+                   NazivGrada = x.Grad.Naziv,
+                   NazivSpol = x.Spol.Naziv,
+                   NazivTeretane = x.Teretana.Naziv,
+                   BrojTelefona = x.BrojTelefona,
+                   Tezina = x.Tezina,
+                   Visina = x.Visina
+
+                }).ToListAsync();
+
+            return new KorisnikGetAllResponse
+            {
+                Korisnici = korisnici
+            };
+
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik/GetAll/KorisnikGetAllResponse.cs
@@ -0,0 +1,32 @@
+﻿using GMS.Entities.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GMS.Entities.Endpoint.Korisnik.GetAll
+{
+    public class KorisnikGetAllResponse
+    {
+        public List<KorisnikGetAllResponseRow> Korisnici { get; set; }
+    }
+
+
+    public class KorisnikGetAllResponseRow // samo se radi kada je rezultat lista a ne jedan zapis 
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string? Slika { get; set; }
+
+        public string BrojTelefona { get; set; }
+        public float Visina { get; set; }
+        public float Tezina { get; set; }
+
+        public string NazivGrada { get; set; } 
+        public string NazivSpol { get; set; } 
+        public string NazivTeretane { get; set; } 
+
+   
+    }
+}
