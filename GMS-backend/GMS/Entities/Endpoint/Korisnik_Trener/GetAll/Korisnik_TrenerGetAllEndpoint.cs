5887aaf2fd3220165b888b0d49bb34d9b0d033d5
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-12 17:53:07 +0100

    Zavrsen sprint

--- GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/GetAll/Korisnik_TrenerGetAllEndpoint.cs
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/GetAll/Korisnik_TrenerGetAllEndpoint.cs
@@ -1,18 +1,10 @@
 ﻿using GMS.Data;
-<<<<<<< HEAD
-=======
-using GMS.Entities.Endpoint.Korisnik_Nutricionist.GetAll;
->>>>>>> 77a924fbcf54ddf95a8e203f21e93249be549661
 using GMS.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace GMS.Entities.Endpoint.Korisnik_Trener.GetAll
 {
-<<<<<<< HEAD
-=======
-
->>>>>>> 77a924fbcf54ddf95a8e203f21e93249be549661
     [Route("Korisnik_Trener-GetAll")]
 
     public class Korisnik_TrenerGetAllEndpoint : MyBaseEndpoint<Korisnik_TrenerGetAllRequest, Korisnik_TrenerGetAllResponse>
@@ -25,17 +17,6 @@ namespace GMS.Entities.Endpoint.Korisnik_Trener.GetAll
         }
 
         [HttpGet]
-<<<<<<< HEAD
-        public override async Task<Korisnik_TrenerGetAllResponse> Handle([FromQuery]Korisnik_TrenerGetAllRequest request)
-        {
-            var korisniktrener = await db.Korisnik_Trener
-                .Select(x => new KorisnikTrenerGetAllResponseRow
-                {
-                    KorisnikID=x.KorisnikID,
-                    TrenerID=x.TrenerID,
-                    OdrzanoSati=x.OdrzanoSati,
-                   
-=======
         public override async Task<Korisnik_TrenerGetAllResponse> Handle([FromQuery] Korisnik_TrenerGetAllRequest request)
         {
             var korisnici_treneri = await db.Korisnik_Trener
@@ -46,16 +27,14 @@ namespace GMS.Entities.Endpoint.Korisnik_Trener.GetAll
                     DatumiVrijemeOdrzavanja = x.DatumTermina,
                     OdrzanoSati = x.OdrzanoSati
 
->>>>>>> 77a924fbcf54ddf95a8e203f21e93249be549661
+
                 }).ToListAsync();
 
             return new Korisnik_TrenerGetAllResponse
             {
-<<<<<<< HEAD
-                KorisnikTrener = korisniktrener
-=======
-                KorisnikTreneri = korisnici_treneri
->>>>>>> 77a924fbcf54ddf95a8e203f21e93249be549661
+
+                KorisnikTrener = korisnici_treneri
+
             };
         }
     }
