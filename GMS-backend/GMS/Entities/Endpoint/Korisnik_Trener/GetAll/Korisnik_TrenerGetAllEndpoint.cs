77a924fbcf54ddf95a8e203f21e93249be549661
Vedad <vedad.ribery@gmail.com>
2023-11-12 17:37:20 +0100

    Update Korisnik_Trener

--- GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/GetAll/Korisnik_TrenerGetAllEndpoint.cs
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/GetAll/Korisnik_TrenerGetAllEndpoint.cs
@@ -1,6 +1,40 @@
-﻿namespace GMS.Entities.Endpoint.Korisnik_Trener.GetAll
+﻿using GMS.Data;
+using GMS.Entities.Endpoint.Korisnik_Nutricionist.GetAll;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Entities.Endpoint.Korisnik_Trener.GetAll
 {
-    public class Korisnik_TrenerGetAllEndpoint
+
+    [Route("Korisnik_Trener-GetAll")]
+
+    public class Korisnik_TrenerGetAllEndpoint : MyBaseEndpoint<Korisnik_TrenerGetAllRequest, Korisnik_TrenerGetAllResponse>
     {
+        private readonly ApplicationDbContext db;
+
+        public Korisnik_TrenerGetAllEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public override async Task<Korisnik_TrenerGetAllResponse> Handle([FromQuery] Korisnik_TrenerGetAllRequest request)
+        {
+            var korisnici_treneri = await db.Korisnik_Trener
+                .Select(x => new KorisnikTrenerGetAllResponseRow
+                {
+                    KorisnikID = x.KorisnikID,
+                    TrenerID = x.TrenerID,
+                    DatumiVrijemeOdrzavanja = x.DatumTermina,
+                    OdrzanoSati = x.OdrzanoSati
+
+                }).ToListAsync();
+
+            return new Korisnik_TrenerGetAllResponse
+            {
+                KorisnikTreneri = korisnici_treneri
+            };
+        }
     }
 }
