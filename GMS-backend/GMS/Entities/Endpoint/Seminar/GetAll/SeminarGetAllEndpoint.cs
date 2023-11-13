c351eb7d06df2cdb1b09a7c0717bc572a03a4f2b
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-13 10:25:28 +0100

    Seminar

---
+++ GMS-backend/GMS/Entities/Endpoint/Seminar/GetAll/SeminarGetAllEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Entities.Endpoint.Seminar.GetAll
+{
+    [Route("Seminar-GetAll")]
+
+    public class SeminarGetAllEndpoint : MyBaseEndpoint<SeminarGetAllRequest, SeminarGetAllResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public SeminarGetAllEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public override async Task<SeminarGetAllResponse> Handle([FromQuery]SeminarGetAllRequest request)
+        {
+            var seminar = await db.Seminar
+                 .Select(x => new SeminarGetAllResponseRow
+                 {
+                     ID = x.ID,
+                     Predavac=x.Predavac,
+                     Datum=x.Datum,
+                     Tema=x.Tema
+                  
+                 }).ToListAsync();
+
+            return new SeminarGetAllResponse
+            {
+                Seminar = seminar
+            };
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Seminar/GetAll/SeminarGetAllResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace GMS.Entities.Endpoint.Seminar.GetAll
+{
+    public class SeminarGetAllResponse
+    {
+        public List<SeminarGetAllResponseRow> Seminar { get; set; }
+    }
+
+    public class SeminarGetAllResponseRow // samo se radi kada je rezultat lista a ne jedan zapis 
+    {
+        public int ID { get; set; }
+        public string Tema { get; set; }
+        public string Predavac { get; set; }
+        public DateTime Datum { get; set; }
+        
+    }
+}
