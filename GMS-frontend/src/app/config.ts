04b02a251f32dd638b1944073c437df591ead1fc
Vedad <vedad.ribery@gmail.com>
2023-11-14 06:16:31 +0100

    Namjestanje konekcijskog stringa za deploy

--- GMS-frontend/src/app/config.ts
+++ GMS-frontend/src/app/config.ts
@@ -1,4 +1,7 @@
 export class Config{
-  static adresa ="https://localhost:44357/";
+  // ---> za lokalno
+   static adresa ="https://localhost:44357/";
+
+  // ---> za deploy
   // static adresa ="https://backend.p2309.app.fit.ba/";
 }
