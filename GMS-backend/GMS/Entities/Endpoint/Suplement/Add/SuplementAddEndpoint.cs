0babb4dd0adf38fe9f3f36d18a3f4f8f5fa83824
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-16 11:27:16 +0100

    Controller za dodavanje suplemenata

---
+++ GMS-backend/GMS/Entities/Endpoint/Suplement/Add/SuplementAddEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Entities.Endpoint.Suplement.Add
+{
+    [Route("Suplement-Add")]
+
+    public class SuplementAddEndpoint : MyBaseEndpoint<SuplementAddRequest, SuplementAddResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public SuplementAddEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public override async Task<SuplementAddResponse> Handle([FromBody]SuplementAddRequest request, CancellationToken cancellationToken)
+        {
+            var novi = new Entities.Models.Suplement
+            {
+                Naziv = request.Naziv,
+                Cijena=request.Cijena,
+                Gramaza=request.Gramaza,
+                Opis=request.Opis,
+                Slika=request.Slika,
+                DobavljacID=request.DobavljacID,
+                KategorijaID=request.KategorijaID
+            };
+
+            db.Suplement.Add(novi);
+            await db.SaveChangesAsync(cancellationToken: cancellationToken);
+
+            return new SuplementAddResponse
+            {
+                ID = novi.ID,
+                Naziv = novi.Naziv,
+                Cijena=novi.Cijena,
+                Gramaza=novi.Gramaza,
+                Opis=novi.Opis,
+                Slika=novi.Slika,
+                DobavljacID=novi.DobavljacID,
+                KategorijaID=novi.KategorijaID
+
+            };
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Suplement/Add/SuplementAddResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace GMS.Entities.Endpoint.Suplement.Add
+{
+    public class SuplementAddResponse
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+        public float Cijena { get; set; }
+        public float Gramaza { get; set; }
+        public string Opis { get; set; }
+        public string? Slika { get; set; }
+        public int DobavljacID { get; set; }
+        public int KategorijaID { get; set; }
+    }
+}
