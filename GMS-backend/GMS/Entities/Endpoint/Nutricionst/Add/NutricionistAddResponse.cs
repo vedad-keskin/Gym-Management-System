c816a624c4b93511491191cde5673914751a6e3e
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-16 19:56:50 +0100

    Controller za dodavanje nutricionista

---
+++ GMS-backend/GMS/Entities/Endpoint/Nutricionst/Add/NutricionistAddResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace GMS.Entities.Endpoint.Nutricionst.Add
+{
+    public class NutricionistAddResponse
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string BrojTelefona { get; set; }
+        public string? Slika { get; set; }
+    }
+}
