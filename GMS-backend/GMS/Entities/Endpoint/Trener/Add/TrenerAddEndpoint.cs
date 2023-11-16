cf324275fc84732fe0e5c49f5b5b4df8bc4b2494
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-16 19:46:57 +0100

    Dodavanje controllera za dodavanje trenera

---
+++ GMS-backend/GMS/Entities/Endpoint/Trener/Add/TrenerAddEndpoint.cs
@@ -0,0 +1,45 @@
+﻿using GMS.Data;
+using GMS.Entities.Endpoint.Suplement.Add;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Entities.Endpoint.Trener.Add
+{
+    [Route("Trener-Add")]
+
+    public class TrenerAddEndpoint : MyBaseEndpoint<TrenerAddRequest, TrenerAddResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public TrenerAddEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public override async Task<TrenerAddResponse> Handle([FromBody]TrenerAddRequest request, CancellationToken cancellationToken)
+        {
+            var novi = new Entities.Models.Trener
+            {
+                Ime=request.Ime,
+                Prezime=request.Prezime,
+                BrojTelefona=request.BrojTelefona,
+                Slika=request.Slika
+                
+            };
+
+            db.Trener.Add(novi);
+            await db.SaveChangesAsync(cancellationToken: cancellationToken);
+
+            return new TrenerAddResponse
+            {
+                ID = novi.ID,
+                Ime=novi.Ime,
+                Prezime=novi.Prezime,
+                BrojTelefona=novi.Prezime,
+                Slika=novi.Slika
+            };
+
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Trener/Add/TrenerAddResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace GMS.Entities.Endpoint.Trener.Add
+{
+    public class TrenerAddResponse
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string BrojTelefona { get; set; }
+        public string? Slika { get; set; }
+    }
+}
