f8bad567891ded68154d0574fcb875c3a1b32c81
Vedad <vedad.ribery@gmail.com>
2023-11-20 21:45:31 +0100

    Namjestanje IP adresa

--- GMS-frontend/src/app/config.ts
+++ GMS-frontend/src/app/config.ts
@@ -1,6 +1,9 @@
 export class Config{
-  // ---> za lokalno
+  // ---> za IIS Express
    static adresa ="https://localhost:44357/";
+
+   // --> za GMS profile
+   // static adresa ="https://localhost:7174/";
 
   // ---> za deploy
   // static adresa ="https://backend.p2309.app.fit.ba/";
