673b61fd4d44234e9c72d8dfcf85eb00c4d8651b
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-20 19:47:07 +0100

    Controller za dodavanje Korisnik_Nutricionist

---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Nutricionist/Add/Korisnik_NutricionistAddEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Entities.Endpoint.Korisnik_Nutricionist.Add
+{
+    [Route("Korisnik_Nutricionist-Add")]
+
+    public class Korisnik_NutricionistAddEndpoint : MyBaseEndpoint<Korisnik_NutricionistAddRequest, Korisnik_NutricionistAddResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public Korisnik_NutricionistAddEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public override async Task<Korisnik_NutricionistAddResponse> Handle([FromBody]Korisnik_NutricionistAddRequest request, CancellationToken cancellationToken)
+        {
+            var novi = new Entities.Models.Korisnik_Nutricionist
+            {
+                KorisnikID=request.KorisnikID,
+                NutricionistID=request.NutricionistID,
+                DatumTermina=request.DatumTermina,
+                OdrzanoSati=request.OdrzanoSati
+            };
+
+            db.Korisnik_Nutricionst.Add(novi);
+            await db.SaveChangesAsync(cancellationToken: cancellationToken);
+
+            return new Korisnik_NutricionistAddResponse
+            {
+                DatumTermina=novi.DatumTermina,
+                OdrzanoSati=novi.OdrzanoSati
+            };
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Nutricionist/Add/Korisnik_NutricionistAddResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace GMS.Entities.Endpoint.Korisnik_Nutricionist.Add
+{
+    public class Korisnik_NutricionistAddResponse
+    {
+        public int KorisnikID { get; set; }
+        public int NutricionistID { get; set; }
+
+        public DateTime DatumTermina { get; set; }
+        public DateTime OdrzanoSati { get; set; }
+    }
+}
