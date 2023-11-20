f3ab6045c9066425f3cf9369b4d2121694bff1f4
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-20 19:47:37 +0100

    Controller za dodavanje Korisnik_Trener

---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/Add/Korisnik_TrenerAddEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Entities.Endpoint.Korisnik_Trener.Add
+{
+    [Route("Korisnik_Trener-Add")]
+
+    public class Korisnik_TrenerAddEndpoint : MyBaseEndpoint<Korisnik_TrenerAddRequest, Korisnik_TrenerAddResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public Korisnik_TrenerAddEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public override async Task<Korisnik_TrenerAddResponse> Handle([FromBody]Korisnik_TrenerAddRequest request, CancellationToken cancellationToken)
+        {
+            var novi = new Entities.Models.Korisnik_Trener
+            {
+                KorisnikID = request.KorisnikID,
+                TrenerID=request.TrenerID,
+                DatumTermina = request.DatumTermina,
+                OdrzanoSati = request.OdrzanoSati
+            };
+
+            db.Korisnik_Trener.Add(novi);
+            await db.SaveChangesAsync(cancellationToken: cancellationToken);
+
+            return new Korisnik_TrenerAddResponse
+            {
+                DatumTermina = novi.DatumTermina,
+                OdrzanoSati = novi.OdrzanoSati
+            };
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/Add/Korisnik_TrenerAddRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace GMS.Entities.Endpoint.Korisnik_Trener.Add
+{
+    public class Korisnik_TrenerAddRequest
+    {
+        public int KorisnikID { get; set; }
+        public int TrenerID { get; set; }
+
+        public DateTime DatumTermina { get; set; }
+        public int OdrzanoSati { get; set; }
+    }
+}
