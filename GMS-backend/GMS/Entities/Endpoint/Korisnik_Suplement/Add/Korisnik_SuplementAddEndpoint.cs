0df4010eb50c63281b4fa21a9ae2fe76c04e5f6b
dzejla01 <dzejla.palalic01@gmail.com>
2023-11-20 19:47:22 +0100

    Controller za dodavanje Korisnik_Suplement

---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Suplement/Add/Korisnik_SuplementAddEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Entities.Endpoint.Korisnik_Suplement.Add
+{
+    [Route("Korisnik_Suplement-Add")]
+
+    public class Korisnik_SuplementAddEndpoint : MyBaseEndpoint<Korisnik_SuplementAddRequest, Korisnik_SuplementAddResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public Korisnik_SuplementAddEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public override async Task<Korisnik_SuplementAddResponse> Handle([FromBody]Korisnik_SuplementAddRequest request, CancellationToken cancellationToken)
+        {
+            var novi = new Entities.Models.Korisnik_Suplement
+            {
+                SuplementID=request.SuplementID,
+                KorisnikID=request.KorisnikID,
+                DatumVrijemeNarudzbe=request.DatumVrijemeNarudzbe,
+                Kolicina=request.Kolicina
+            };
+
+            db.Korisnik_Suplement.Add(novi);
+            await db.SaveChangesAsync(cancellationToken: cancellationToken);
+
+            return new Korisnik_SuplementAddResponse
+            {
+               DatumVrijemeNarudzbe=novi.DatumVrijemeNarudzbe,
+               Kolicina=novi.Kolicina
+            };
+        }
+    }
+}
