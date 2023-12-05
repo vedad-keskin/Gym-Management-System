5a828613bf984780e0c67389c1ea015cf8a038c4
Vedad <vedad.ribery@gmail.com>
2023-12-05 01:44:47 +0100

    Module

--- GMS-frontend/src/app/app.module.ts
+++ GMS-frontend/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {FaqPageComponent } from './faq-page/faq-page.component';
 import {OsobljePageComponent} from "./osoblje-page/osoblje-page.component";
 import {SuplementiPageComponent} from "./suplementi-page/suplementi-page.component";
-import {HomePageComponent} from "./home-page/home-page.component";
 import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import { LoginPageComponent } from './login-page/login-page.component';
 import {MyAuthInterceptor} from "./helpers/auth/my-auth-interceptor-service";
+import {FaqPageComponent} from "./faq-page/faq-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 import {KorisnikPageComponent} from "./korisnik-page/korisnik-page.component";
 import {AdministratorPageComponent} from "./administrator-page/administrator-page.component";
+import {AutorizacijaGuardKorisnik} from "./helpers/auth/autorizacija-guard-korisnik.service";
+import {
+    AutorizacijaGuardAdministrator
+} from "./helpers/auth/autorizacija-guard-administrator.service";
 
 
 
@@ -40,12 +44,14 @@ import {AdministratorPageComponent} from "./administrator-page/administrator-pag
       {path:'OsobljePage', component: OsobljePageComponent },
       {path:'SuplementiPage', component: SuplementiPageComponent },
       {path:'LoginPage', component: LoginPageComponent },
-      {path:'KorisnikPage', component: KorisnikPageComponent },
-      {path:'AdministratorPage', component: AdministratorPageComponent }
+      {path:'KorisnikPage', component: KorisnikPageComponent, canActivate: [AutorizacijaGuardKorisnik] },
+      {path:'AdministratorPage', component: AdministratorPageComponent, canActivate: [AutorizacijaGuardAdministrator]  }
     ])
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: MyAuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: MyAuthInterceptor, multi: true },
+      AutorizacijaGuardKorisnik,
+      AutorizacijaGuardAdministrator
   ],
   bootstrap: [AppComponent]
 })
