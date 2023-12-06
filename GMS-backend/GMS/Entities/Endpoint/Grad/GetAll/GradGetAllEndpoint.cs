d9d92c11ec44e283df2f71b9e0c51dfc4385db56
dzejla01 <dzejla.palalic01@gmail.com>
2023-12-06 20:42:49 +0100

    GradGetAll

---
+++ GMS-backend/GMS/Entities/Endpoint/Grad/GetAll/GradGetAllEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Entities.Endpoint.Grad.GetAll
+{
+    [Route("Grad-GetAll")]
+
+    public class GradGetAllEndpoint : MyBaseEndpoint<GradGetAllRequest, GradGetAllResponse>
+    {
+        private readonly ApplicationDbContext db;
+
+        public GradGetAllEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public override async Task<GradGetAllResponse> Handle([FromQuery]GradGetAllRequest request, CancellationToken cancellationToken)
+        {
+            var grad = await db.Grad
+                .Select(x => new GradGetAllResponseRow
+                {
+                    ID = x.ID,
+                    Naziv = x.Naziv,
+                    
+
+
+                }).ToListAsync(cancellationToken: cancellationToken);
+
+            return new GradGetAllResponse
+            {
+                Grad = grad
+            };
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Grad/GetAll/GradGetAllResponse.cs
@@ -0,0 +1,14 @@
+﻿
+namespace GMS.Entities.Endpoint.Grad.GetAll
+{
+    public class GradGetAllResponse
+    {
+        public List<GradGetAllResponseRow> Grad { get; set; }
+    }
+
+    public class GradGetAllResponseRow // samo se radi kada je rezultat lista a ne jedan zapis 
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+    }
+}
