de24e95035766d1b60eebe01c6b45cc346e91713
dzejla01 <dzejla.palalic01@gmail.com>
2023-12-15 11:50:02 +0100

    Recenzija Add i Edit endpointi

---
+++ GMS-backend/GMS/Entities/Endpoint/Recenzija/Add/RecenzijaAddResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace GMS.Entities.Endpoint.Recenzija.Add
+{
+    public class RecenzijaAddResponse
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Zanimanje { get; set; }
+        public string Tekst { get; set; }
+        public string? Slika { get; set; }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Recenzija/Edit/RecenzijeEditEndpoint.cs
@@ -0,0 +1,50 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using GMS.Helpers.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Entities.Endpoint.Recenzija.Edit
+{
+    [Route("Recenzija-Edit")]
+    [MyAuthorization]
+
+    public class RecenzijeEditEndpoint : MyBaseEndpoint<RecenzijeEditRequest, int>
+    {
+        private readonly ApplicationDbContext db;
+
+        public RecenzijeEditEndpoint(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public override async Task<int> Handle([FromBody]RecenzijeEditRequest request, CancellationToken cancellationToken)
+        {
+            Models.Recenzija? recenzija;
+            if (request.ID == 0)
+            {
+                recenzija = new Models.Recenzija();
+                db.Add(recenzija);
+
+
+            }
+            else
+            {
+                recenzija = db.Recenzija.FirstOrDefault(s => s.ID == request.ID);
+                if (recenzija == null)
+                    throw new Exception("pogresan ID");
+            }
+
+            recenzija.Ime = request.Ime.RemoveTags();
+            recenzija.Prezime = request.Prezime.RemoveTags();
+            recenzija.Zanimanje = request.Zanimanje.RemoveTags();
+            recenzija.Tekst = request.Tekst.RemoveTags();
+            recenzija.Slika = request.Slika.RemoveTags();
+
+
+            await db.SaveChangesAsync(cancellationToken);
+
+            return recenzija.ID;
+        }
+    }
+}
