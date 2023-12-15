cdaa195aef952540991c4edda1302158cb47edb2
dzejla01 <dzejla.palalic01@gmail.com>
2023-12-15 11:49:42 +0100

    GetById KorisnikTrener

---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/GetById/KorisnikTrenerGetEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using GMS.Data;
+using GMS.Helpers;
+using GMS.Helpers.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Entities.Endpoint.Korisnik_Trener.GetById
+{
+    [Route("Korisnik-Trener-Get")]
+
+    public class KorisnikTrenerGetEndpoint : MyBaseEndpoint<int, KorisnikTrenerGetResponse>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly MyAuthService _authService;
+
+        public KorisnikTrenerGetEndpoint(ApplicationDbContext applicationDbContext, MyAuthService authService)
+        {
+            _applicationDbContext = applicationDbContext;
+            _authService = authService;
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<KorisnikTrenerGetResponse> Handle(int id, CancellationToken cancellationToken)
+        {
+            var korisnik = await _applicationDbContext.Korisnik.FindAsync(id);
+
+            if (korisnik is null)
+                throw new Exception("Nije nađen korisnik za id = " + id);
+
+            var result = new KorisnikTrenerGetResponse
+            {
+                KorisnikID = korisnik.ID,
+                Ime = korisnik.Ime,
+                Prezime = korisnik.Prezime,
+                ZakazaniTermini = await _applicationDbContext
+                    .Korisnik_Trener.Include("Trener").Include("Korisnik")
+                    .Where(x => x.KorisnikID == id)
+                    .Select(x => new KorisnikTrenerGetResponseZakazaniTermini
+                    {
+                        KorisnikID=x.KorisnikID,
+                        TrenerID=x.TrenerID,
+                        DatumTermina=x.DatumTermina,
+                        OdrzanoSati=x.OdrzanoSati
+
+                    })
+                    .ToListAsync(cancellationToken)
+            };
+            return result;
+        }
+    }
+}
---
+++ GMS-backend/GMS/Entities/Endpoint/Korisnik_Trener/GetById/KorisnikTrenerGetResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace GMS.Entities.Endpoint.Korisnik_Trener.GetById
+{
+    public class KorisnikTrenerGetResponse
+    {
+        public int KorisnikID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public List<KorisnikTrenerGetResponseZakazaniTermini> ZakazaniTermini { get; set; }
+    }
+
+    public class KorisnikTrenerGetResponseZakazaniTermini
+    {
+        public int KorisnikID { get; set; }
+        public int TrenerID { get; set; }
+        public int OdrzanoSati { get; set; }
+        public DateTime DatumTermina { get; set; }
+    }
+}
