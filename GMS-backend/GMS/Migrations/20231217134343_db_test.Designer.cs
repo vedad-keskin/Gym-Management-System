109e9980cf22ea2fdedab442cc105b69ac540fa7
dzejla01 <dzejla.palalic01@gmail.com>
2023-12-17 14:50:11 +0100

    Migracija i sredjivanje nutricioniste

--- GMS-backend/GMS/Migrations/20231217134343_db_test.Designer.cs
+++ GMS-backend/GMS/Migrations/20231217134343_db_test.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GMS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20231217015647_init")]
-    partial class init
+    [Migration("20231217134343_db_test")]
+    partial class db_test
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
